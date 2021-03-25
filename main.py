6e9be4d8ed96747dcc55a841e59cd07b1925fe18
Ehsan Shaghaei <ehsan2754@github.com>
2021-03-25 10:36:56 +0300

    Encapuslation, Database restructuring, Image construction

--- main.py
+++ main.py
@@ -1,15 +1,24 @@
 import time, os, json, base64
-import src
+from io import BytesIO
 from datetime import datetime
-from flask import Flask, render_template, url_for, request, redirect
+from flask import Flask, render_template, url_for, request, redirect,session
 from flask_ngrok import run_with_ngrok
 from flask_sqlalchemy import SQLAlchemy
+import numpy as np
+from math import factorial
+from scipy.special import assoc_laguerre as lg
+import matplotlib.pyplot as plt
+from matplotlib import cm
+from matplotlib.ticker import LinearLocator, FormatStrFormatter
+from matplotlib.figure import Figure
+import src
 # from flask_restful import Api,Resource
 # ----------- Global Variables
 app = Flask(__name__)
 run_with_ngrok(app)
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///history.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///QantumComputingLab.db'
+app.config['SECRET_KEY'] = 'w276-T#treBxTpY7X-jZZ96@tAHHtw5F'
 db = SQLAlchemy(app)
 DEFAULT_IMG = 'img/default.jpg'
 TMP_IMG = 'img/tmp.bmp'
@@ -19,66 +28,124 @@ SAVED = True
 
 # ----------- BOT-ACTIVATION
 # ----------- Classes
-class History(db.Model):
-    # --- TEMPERORAY STATUS IN DB
-    status = SAVED
+class Superposition(db.Model):
+
+    ### DATABASE attributes
+    __tablename__ = 'Superposition'
     # Identification
     id = db.Column(db.Integer, primary_key=True)
     # --- Destination SLM
-    dest = db.Column(db.String(15), nullable=True)
-    # --- Gaussian waist
-    gussianWaist = db.Column(db.Float, nullable=False)
+    destination = db.relationship('Devices', backref='Superposition')
+    # --- Height
+    height = db.Column(db.Integer, nullable=False)
+    # --- Width
+    width  = db.Column(db.Integer, nullable=False)
+    # --- Gaussian waist "W"
+    gussian_waist = db.Column(db.Float, nullable=False)
+    # --- Radial Index
+    radial_index = db.Column(db.Float, nullable=False)
     # --- Number of parameters for LG
-    nValues = db.Column(db.Integer, nullable=False)
+    n_values = db.Column(db.Integer, nullable=False)
     # --- Values for LG parameters
-    values = db.relationship('Values', backref='owner')
+    values = db.relationship('LG_Values', backref='Superposition')
     # --- Scanning position
-    scanPos = db.Column(db.Integer, nullable=False)
+    h_shift = db.Column(db.Integer, nullable=False)
     # --- Scanning range
-    scanRange = db.Column(db.Integer, nullable=False)
+    v_shift = db.Column(db.Integer, nullable=False)
+    # --- Blaze Period
+    blaze_period = db.Column(db.Float)
     # --- Image file
     image = db.Column(db.LargeBinary)
     # --- Creation date
     date = db.Column(db.DateTime, default=datetime.utcnow)
+
 
     # TO-DO :
     # add the scanning parameters(x,y)
     # image
     # loading option
     def __repr__(self):
-        return '''
-        <{0}>
-        <{1}>
-        <{2}>
-        <{3}>
-        <{4}>
-        <{5}>
-        <{6}>
-        <{7}>
-        '''.format(self.id, self.date, self.dest,
-                   self.gussianWaist, self.scanPos, self.scanRange,
-                   str(self.nValues), self.values)
+        return f'''
+        <{id}>
+        <{destination.id}>
+        <{gussian_waist}>
+        <{radial_index}>
+        <{n_values}>
+        <{values}>
+        <{h_shift}>
+        <{v_shift}>
+        <{blaze_period}>
+        <{date}>
+        '''
 
-    def processImage(self):
-        # Create image
-        self.image = src.getB64string(open('Diagram.jpg', 'rb').read())
+    def get_LG_pairs(self,values=self.values):
+        buffer = {t.index:(t.parameter,t.coefficient) for t in values}
+        buffer_s = {k: buffer[k] for k in sorted(buffer)}
+        return [buffer_s[i] for i in buffer_s]
 
+    def getAxis(self,magnitute,shift):
+        return np.linspace(-1*magnitute/2+shift,magnitute/2+shift-1,magnitute)
 
-class Values(db.Model):
-    status = SAVED
+    def car2polar(self,x,y):
+        rho = np.hypot(x,y)
+        phi = np.arctan2(y, x)
+        return rho, phi
+
+    def Lpl(self, x,l,p):
+        value = 0
+        for i in range(p+1):
+            value = value+((-1)**(i))*factorial(p+abs(l))/factorial(p-i)/factorial(abs(l)+i)/factorial(i)*x**i
+        return value
+
+    def N_PL(self,p,l):
+        return np.sqrt(factorial(p)/factorial(p+abs(l)))
+
+    def lagurrelGussian(self,rho,phi,w,l,p):
+        rho2w = rho/w
+        return N_PL(p,l)*np.power(np.sqrt(2)*rho2w,abs(l))*lg(2*np.power(rho2w,2),p,abs(l))*np.exp(-1*np.power(rho2w,2))*np.exp(complex(0,-1)*l*phi)
+
+    def get_Superposition(self,width=self.width,height=self.height,h_shift=self.h_shift,v_shift=self.v_shift,w=self.gussian_waist,p=self.radial_index,values=self.get_LG_pairs()):
+        x, y     = getAxis(width,h_shift),getAxis(height,v_shift)
+        xv, yv   = np.meshgrid(x,y)
+        rho, phi = car2polar(xv,yv)
+        out  = 0
+        for m,n in values:
+            temp = n*lagurrelGussian(rho,phi,w,m,p)
+            out += temp  
+        return out 
+        
+    
+    def get_Superposition_image(self,width=self.width,height=self.height,h_shift=self.h_shift,v_shift=self.v_shift,w=self.gussian_waist,p=self.radial_index,values=self.get_LG_pairs(),ri=self.radial_index,scale=255):
+        out = self.get_Superposition(width,height,h_shift,v_shift,w,p,values)
+        x, y     = getAxis(width,h_shift),getAxis(height,v_shift)
+        xv, yv   = np.meshgrid(x,y)
+        amp   = np.absolute(out)
+        phase = np.angle(out)
+        amp=amp/np.amax(amp)
+        phase_mod = (phase+2*np.pi*xv/ri) % (2*np.pi)   
+        result = amp*phase_mod
+        scaled_result = result/result.max()*scale
+        buf = BytesIO()
+        plt.imsave(buf,scaled_result, cmap="gray",origin='lower', vmin = 0,vmax=scale,format="png")
+        return base64.b64encode(buf.getbuffer()).decode("ascii")
+
+class Devices(db.Model):
+    __tablename__ = 'Devices'
     id = db.Column(db.Integer, primary_key=True)
-    history_id = db.Column(db.Integer, db.ForeignKey('history.id'))
+    Superposition_id = Column(Integer, ForeignKey('Superposition.id'))
+    
+
+class LG_Values(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    Superposition_id = db.Column(db.Integer, db.ForeignKey('Superposition.id'))
+    index = db.Column(db.Integer, nullable=False)
     coefficient = db.Column(db.Float, nullable=False)
     parameter = db.Column(db.Float, nullable=False)
-    index = db.Column(db.Integer, nullable=False)
-
     def __repr__(self):
         return '<i={},p={},c={}>'.format(self.index, self.parameter,
                                          self.coefficient)
-
-
 # ----------- Constants
-INIT_form = History(id=0,
+INIT_form = Superposition(id=0,
                     gussianWaist=None,
                     nValues=1,
                     values=[Values(parameter=None, coefficient=None)],
@@ -86,7 +153,7 @@ INIT_form = History(id=0,
                     scanRange=None,
                     image=src.getB64string(
                         open('static/img/default.jpg', 'rb').read()))
-TMP_history = History()
+TMP_SP = Superposition()
 TMP_values = []
 
 
